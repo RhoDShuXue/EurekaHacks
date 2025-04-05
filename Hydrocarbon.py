e44d12d6d801f6c9782983e7c0a7bbe499f79faa
RhoDShuXue <georgeyytt@gmail.com>
2025-04-05 15:44:34 -0400

    Commit 13

--- Hydrocarbon.py
+++ Hydrocarbon.py
@@ -7,49 +7,53 @@ matplotlib.use("TkAgg")
 
 
 
-hydro_particles = 100
-oxy_particles = 50
+light = 20
+heavy = 20
 container_size = 1
 particles_speed = 0.1
+target_y = 0.5
 
-hydro_pos = np.random.rand(hydro_particles, 2) * container_size
-hydroV = np.random.rand(hydro_particles, 2) * particles_speed
-oxy_pos = np.random.rand(oxy_particles, 2) * container_size
-oxyV = np.random.rand(oxy_particles, 2) * particles_speed
+light_pos = np.random.rand(light, 2) * container_size
+lightV = np.array([[0, particles_speed] for _ in range(light)])
+heavy_pos = np.random.rand(heavy, 2) * container_size
+heavyV = np.zeros((heavy, 2))
+
+for i in range(heavy):
+    heavyV[i, 1] = (target_y - heavy_pos[i, 1]) * particles_speed  # Move towards target y
 
 collisions = []
 
-fig ,ax = plt.subplots()
-hydroScatter = ax.scatter(hydro_pos[: ,0], hydro_pos[: ,1], color = "gray", marker = "_")
-oxyScatter = ax.scatter(oxy_pos[: ,0], oxy_pos[: ,1], color = "gray", marker = "o")
+fig, ax = plt.subplots()
+light_scatter = ax.scatter(light_pos[:, 0], light_pos[:, 1], color="grey", marker="o")
+heavy_scatter = ax.scatter(heavy_pos[:, 0], heavy_pos[:, 1], color="grey", marker="_")
 ax.set_xlim(0, container_size)
 ax.set_ylim(0, container_size)
-plt.gca().set_aspect("equal", adjustable = "box")
+plt.gca().set_aspect("equal", adjustable="box")
 
 plt.xticks([])
 plt.yticks([])
 
 for steps in range(500):
-    hydro_pos += hydroV
-    oxy_pos += oxyV
+    light_pos += lightV
+    heavy_pos += heavyV
     num_collision = 0
 
-    for a in range(hydro_particles):
+    for a in range(light):
         for b in range(2):
-            if hydro_pos[a, b] < 0 or hydro_pos[a, b] > 1:
-                hydroV[a, b] *= -1
+            if light_pos[a, b] < 0 or light_pos[a, b] > 1:
+                lightV[a, b] *= -1
                 num_collision += 1
 
-    for c in range(oxy_particles):
+    for c in range(heavy):
         for d in range(2):
-            if oxy_pos[c, d] < 0 or oxy_pos[c, d] > 1:
-                oxyV[c, d] *= -1
+            if heavy_pos[c, d] < 0 or heavy_pos[c, d] > 1:
+                heavyV[c, d] *= -1
                 num_collision += 1
 
     collisions = np.append(collisions, num_collision)
 
-    hydroScatter.set_offsets(hydro_pos)
-    oxyScatter.set_offsets(oxy_pos)
+    light_scatter.set_offsets(light_pos)
+    heavy_scatter.set_offsets(heavy_pos)
     plt.pause(0.1)
 
 plt.show()
