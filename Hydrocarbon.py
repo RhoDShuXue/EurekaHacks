c197bf309dcbea1fd9eb677c3555b4f0b23ba0d9
RhoDShuXue <georgeyytt@gmail.com>
2025-04-05 15:52:18 -0400

    Commit 14

--- Hydrocarbon.py
+++ Hydrocarbon.py
@@ -5,8 +5,6 @@ import pandas as pd
 import matplotlib
 matplotlib.use("TkAgg")
 
-
-
 light = 20
 heavy = 20
 container_size = 1
@@ -33,7 +31,7 @@ plt.gca().set_aspect("equal", adjustable="box")
 plt.xticks([])
 plt.yticks([])
 
-for steps in range(500):
+for steps in range(50):
     light_pos += lightV
     heavy_pos += heavyV
     num_collision = 0
@@ -55,5 +53,34 @@ for steps in range(500):
     light_scatter.set_offsets(light_pos)
     heavy_scatter.set_offsets(heavy_pos)
     plt.pause(0.1)
+
+lightV[:, 1] = -particles_speed
+heavyV[:, 1] = -particles_speed
+
+while True:
+    light_pos += lightV
+    heavy_pos += heavyV
+    num_collision = 0
+
+    for a in range(light):
+        for b in range(2):
+            if light_pos[a, b] < 0 or light_pos[a, b] > 1:
+                lightV[a, b] *= -1
+                num_collision += 1
+
+    for c in range(heavy):
+        for d in range(2):
+            if heavy_pos[c, d] < 0 or heavy_pos[c, d] > 1:
+                heavyV[c, d] *= -1
+                num_collision += 1
+
+    collisions = np.append(collisions, num_collision)
+
+    light_scatter.set_offsets(light_pos)
+    heavy_scatter.set_offsets(heavy_pos)
+    plt.pause(0.1)
+
+    if np.all(light_pos[:, 1] <= 0) and np.all(heavy_pos[:, 1] <= 0):
+        break
 
 plt.show()
