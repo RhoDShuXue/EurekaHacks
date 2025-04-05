f33878a7c813969ff0be1811841f98d76348f81f
RhoDShuXue <georgeyytt@gmail.com>
2025-04-05 13:52:15 -0400

    Commit 9

--- gas.py
+++ gas.py
@@ -9,7 +9,6 @@ matplotlib.use("TkAgg")
 
 num_particles = 100
 container_size = 1
-time_steps = 1000
 particles_speed = 0.1
 
 positions = np.random.rand(num_particles, 2) * container_size
@@ -21,19 +20,24 @@ ax.set_xlim(0, container_size)
 ax.set_ylim(0, container_size)
 plt.gca().set_aspect("equal", adjustable = "box")
 
+collisions = []
+
 plt.xticks([])
 plt.yticks([])
 
-for steps in range(time_steps):
+for steps in range(1000):
     positions += velocity
+    num_collision = 0
 
     for i in range(num_particles):
         for j in range(2):
             if positions[i,j] < 0 or positions[i,j] > 1:
                 velocity[i,j] *= -1
+                num_collision += 1
+
+    collisions = np.append(collisions, num_collision)
 
     scatter.set_offsets(positions)
     plt.pause(0.1)
-
 
 plt.show()
