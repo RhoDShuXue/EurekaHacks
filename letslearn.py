64e7b407642f0b96a39dffe403dee31e2085ec2b
RhoDShuXue <georgeyytt@gmail.com>
2025-04-05 10:36:05 -0400

    Commit 1

---
+++ letslearn.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+x1 = [1,2,3,4]
+y1 = [2,4,6,8]
+
+x2 = np.arange(0,5,0.25)
+
+
+
+plt.title("I'm Crashing Out", fontdict = {"fontsize": 20})
+plt.xlabel("x-axis")
+plt.ylabel("y-axis")
+
+plt.plot(x1,y1, color = "red", marker = '.', markersize = '10')
+plt.plot(x2,x2 ** (1/2), "b.-")
+#Shorthand: 'r.-'
+
+plt.show()
